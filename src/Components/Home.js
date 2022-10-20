28f1fcad643905d9ab39f4d8214e7ff1fe427f40
Abhishek Shukla <90856267+abhishek2002Git@users.noreply.github.com>
2022-10-20 16:26:04 +0530

    project section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -96,7 +96,9 @@ const Home = () => {
             </button>
           </div>
           <div className="skills-section lg:w-[50%] w-[90vw] mt-14 h-[300px] md:h-[470px]">
-            <h2 className="text-[21px] md:text-[28px] font-[700] mb-[1rem] md:mb-[2rem]">My Skills</h2>
+            <h2 className="text-[21px] md:text-[28px] font-[700] mb-[1rem] md:mb-[2rem]">
+              My Skills
+            </h2>
             <div className="skill-boxes">
               <button className="skill-box text-[14px] md:text-[16px] text-[#666666] bg-[#99999933]  font-[600]  rounded-[5px] py-[0.5em] px-[1.5em] mr-[1rem] mb-[1.5rem]">
                 HTML
@@ -105,7 +107,7 @@ const Home = () => {
                 CSS
               </button>
               <button className="skill-box text-[14px] md:text-[16px] text-[#666666] bg-[#99999933]  font-[600]  rounded-[5px] py-[0.5em] px-[1.5em] mr-[1rem] mb-[1.5rem]">
-              JavaScript
+                JavaScript
               </button>
               <button className="skill-box text-[14px] md:text-[16px] text-[#666666] bg-[#99999933]  font-[600]  rounded-[5px] py-[0.5em] px-[1.5em] mr-[1rem] mb-[1.5rem]">
                 ReactJs
@@ -132,6 +134,54 @@ const Home = () => {
                 NodeJs Basics
               </button>
             </div>
+          </div>
+        </div>
+      </section>
+
+      {/* ************************ Projects Section *********************/}
+      <section className="flex flex-col items-center py-[75px] md:py-[100px]">
+        <h1 className="about-me-title text-[#111111] font-[800] tracking-[3px]">
+          PROJECTS
+        </h1>
+        <div className="bg-[#E53935] w-[30px] h-[5px] my-[13px]"></div>
+        <p className="text-[#555555] text-[16px] leading-[1.6] w-[92%] max-w-[49rem] font-[500] text-center md:text-[18px]">
+          Here you will find some of the personal and clients projects that I
+          created with each project containing its own case study
+        </p>
+
+        <div className="project flex md:flex-row flex-col pt-[65px] md:pt-[90px]">
+          <div className="md:w-[55%] w-[100%] flex md:justify-end justify-center">
+            <img
+              className="w-[90%]"
+              src="https://d33wubrfki0l68.cloudfront.net/5b377ce34a47fc89fc8d33869f2342518a604589/71ecb/assets/jpeg/yubter.jpeg"
+              alt=""
+            />
+          </div>
+          <div className="w-[100%] md:w-[45%] flex flex-col justify-center md:items-start items-center md:pl-10">
+            <h2 className="text-[26px] font-[700] mb-[1rem]">Yubter</h2>
+            <p className="text-[16px] md:text-[17px] text-[#666666] mb-[2rem] leading-[1.7] max-w-[30rem] px-3">
+              Yubter is a platform that I created where people can join the
+              community of their favorite youtube channels and can be part of
+              the conversation.
+            </p>
+            <button className="project-btn text-[13px] md:text-[15px] text-[#ffffff] bg-[#E53539] tracking-[1px] font-[700] rounded-[5px] py-[1em] px-[3.3em]">CASE STUDY</button>
+          </div>
+        </div>
+        
+        <div className="project flex md:flex-row flex-col pt-[65px] md:pt-[90px]">
+          <div className="md:w-[55%] w-[100%] flex md:justify-end justify-center">
+            <img
+              className="w-[90%]"
+              src="https://d33wubrfki0l68.cloudfront.net/61fc288f39a306e3130c72643480ce015dec48ae/e3272/assets/jpeg/boreal-coffee-clone.jpeg"
+              alt=""
+            />
+          </div>
+          <div className="w-[100%] md:w-[45%] flex flex-col justify-center md:items-start items-center md:pl-10">
+            <h2 className="text-[26px] font-[700] mb-[1rem]">Boreal Coffee Clone</h2>
+            <p className="text-[16px] md:text-[17px] text-[#666666] mb-[2rem] leading-[1.7] max-w-[30rem] px-3">
+            I re-created the frontend of Boreal Coffee's official web app because I got attracted to their beautiful UI. It was a great experience for me to build the entire frontend.
+            </p>
+            <button className="project-btn text-[13px] md:text-[15px] text-[#ffffff] bg-[#E53539] tracking-[1px] font-[700] rounded-[5px] py-[1em] px-[3.3em]">CASE STUDY</button>
           </div>
         </div>
       </section>
